7ce9cd8cb9e29ad3e0f662cd3806d9edbbb7eec9
hill <hill@techfak.uni-bielefeld.de>
2023-04-10 00:16:36 +0200

    Add game logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,49 +5,52 @@ const boardWidth : number = 5
 const boardHeight : number = 5
 const winLength : number = 4
 
-// Calculate winning combinations
-const winningCombinations : number[][] = []
-for (let i = 0; i < boardHeight; i++) {
-  for (let j = 0; j < boardWidth; j++) {
-    // horizontal
-    if (j + winLength <= boardWidth) {
-      let combination : number[] = []
-      for (let k = 0; k < winLength; k++) {
-        combination.push(i * boardWidth + j + k)
+// Calculate winning combinations based on board size and win length
+function calculateWinningCombinations(boardWidth : number, boardHeight : number, winLength : number) : number[][] {
+  let winningCombinations : number[][] = []
+  for (let i = 0; i < boardHeight; i++) {
+    for (let j = 0; j < boardWidth; j++) {
+      // horizontal
+      if (j + winLength <= boardWidth) {
+        let combination : number[] = []
+        for (let k = 0; k < winLength; k++) {
+          combination.push(i * boardWidth + j + k)
+        }
+        winningCombinations.push(combination)
       }
-      winningCombinations.push(combination)
-    }
 
-    // vertical
-    if (i + winLength <= boardHeight) {
-      let combination : number[] = []
-      for (let k = 0; k < winLength; k++) {
-        combination.push((i + k) * boardWidth + j)
+      // vertical
+      if (i + winLength <= boardHeight) {
+        let combination : number[] = []
+        for (let k = 0; k < winLength; k++) {
+          combination.push((i + k) * boardWidth + j)
+        }
+        winningCombinations.push(combination)
       }
-      winningCombinations.push(combination)
-    }
 
-    // diagonal
-    if (i + winLength <= boardHeight && j + winLength <= boardWidth) {
-      let combination : number[] = []
-      for (let k = 0; k < winLength; k++) {
-        combination.push((i + k) * boardWidth + j + k)
+      // diagonal
+      if (i + winLength <= boardHeight && j + winLength <= boardWidth) {
+        let combination : number[] = []
+        for (let k = 0; k < winLength; k++) {
+          combination.push((i + k) * boardWidth + j + k)
+        }
+        winningCombinations.push(combination)
       }
-      winningCombinations.push(combination)
-    }
 
-    // anti-diagonal
-    if (i + winLength <= boardHeight && j - winLength + 1 >= 0) {
-      let combination : number[] = []
-      for (let k = 0; k < winLength; k++) {
-        combination.push((i + k) * boardWidth + j - k)
+      // anti-diagonal
+      if (i + winLength <= boardHeight && j - winLength + 1 >= 0) {
+        let combination : number[] = []
+        for (let k = 0; k < winLength; k++) {
+          combination.push((i + k) * boardWidth + j - k)
+        }
+        winningCombinations.push(combination)
       }
-      winningCombinations.push(combination)
     }
   }
+  return winningCombinations
 }
 
-// Check if the board has a winning combination
+// Check if the board has a winning combination and return winner
 function calculateWinner(board: string[], winningCombinations: number[][]) {
   for (let combination of winningCombinations) {
     let combinationExists = true
@@ -63,7 +66,7 @@ function calculateWinner(board: string[], winningCombinations: number[][]) {
     }
     if (combinationExists) {
       console.log(combination)
-      return combination
+      return combination[0]
     }
   }
   return null
@@ -86,14 +89,23 @@ function Square({ value, onClick } : { value : string, onClick : () => void}) {
 
 function App() {
   const [board, setBoard] = useState(Array(boardWidth * boardHeight).fill(null))
+  const [isXTurn, setIsXTurn] = useState(true)
+  const [winner, setWinner] = useState('')
+  const winningCombinations = calculateWinningCombinations(boardWidth, boardHeight, winLength)
 
-  function handleClick(i : number) {
-    const nextBoard = board.slice()
-    nextBoard[i] = 'X'
-    if (calculateWinner(nextBoard, winningCombinations)) {
-      alert('You won!')
+  function handleSquareClick(i : number) {
+    if (board[i] !== null || winner !== '') {
+      return
     }
+    const nextBoard = board.slice()
+    let currentPlayer = isXTurn ? 'X' : 'O'
+    nextBoard[i] = currentPlayer
     setBoard(nextBoard)
+    if (calculateWinner(nextBoard, winningCombinations)) {
+      setWinner(currentPlayer)
+      alert('Winner is ' + currentPlayer + '!')
+    }
+    setIsXTurn(!isXTurn)
   }
 
   let squares : JSX.Element[] = []
@@ -101,7 +113,7 @@ function App() {
     let row : JSX.Element[] = []
     for (let j = 0; j < boardWidth; j++) {
       const index = i * boardWidth + j
-      row.push(<Square key={index} value={board[index]} onClick={() => handleClick(index)} />)
+      row.push(<Square key={index} value={board[index]} onClick={() => handleSquareClick(index)} />)
     } 
     squares.push(<div className="flex">{row}</div>)
   }
@@ -110,6 +122,11 @@ function App() {
     <>
       <Switch />
       {squares}
+      <button onClick={() => {
+        setBoard(Array(boardWidth * boardHeight).fill(null))
+        setIsXTurn(true)
+        setWinner('')
+      }}>Reset Game</button>
     </>
   )
 }
