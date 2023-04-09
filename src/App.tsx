797cf11241b91949fe30e402000524ceb7e9d5de
hill <hill@techfak.uni-bielefeld.de>
2023-04-09 16:38:06 +0200

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </>
+  )
+}
+
+export default App
